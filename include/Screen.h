76fff6670a96006e4833be963300aee66f558a90
Luke Southam <luke@devthe.com>
2013-10-13 21:06:54 +0100

    Fixed formatting.

--- include/Screen.h
+++ include/Screen.h
@@ -13,7 +13,8 @@ public:
     void SetPixel(int x, int y, int c);                         // Set the colour of a pixel on the screen, using an integer colour
     int GetPixel(int x, int y);                                 // Returns the integer colour of a pixel on the screen
     void Clear(int c);                                          // Clear the screen with a solid colour
-    void DrawRectangle(int x, int y, int w, int h, int fc, int bc, int bt); // fc: fill colour, bc: border colour, bt: border thickness
+    void DrawRectangle(int x, int y, int w, int h, int fc, int bc, int bt);
+                                                                // fc: fill colour, bc: border colour, bt: border thickness
     void DrawCircle(int x, int y, int r, int c);                // Draw a filled circle with radius r
     void DrawLine(int x0, int y0, int x1, int y1, int c);       // Draw a line between (x0, y0) and (x1, y1)
     SDL_Surface* GetSurface(void);                              // Returns a pointer to the SDL_Surface

f3d391650dd04b83b6b5b7a1e69758dd3a334194
Isaac Polloni <yuiop1620@hotmail.co.uk>
2013-10-14 19:26:30 +0100

    Added HASFLAG and experimental DECFLAG
    These would be used in the following manner:
    enum
    {
    DECFLAG(VALUE_1) // Is equal to 1
    DECFLAG(VALUE_2) // Is equal to 2
    DECFLAG(VALUE_3) // Is equal to 4            <---
    };
    int i = 5;
    if (HASFLAG(i, VALUE_1))
    std::cout << "Contains i has the VALUE_1 flag";

--- include/stdafx.h
+++ include/stdafx.h
@@ -11,6 +11,9 @@ typedef unsigned long long  QWORD;      // A 64-bit word
 
 #define PI                  3.1415926535897
 
+#define HASFLAG(v, f)       v & f == f  // Check to see if value, v, contains flag, f (Use in place of boolean expression)
+#define DECFLAG(f)          f = 1 << f  // [Experimental] Declare an enum member with a value suitable for being a flag
+
 #include <stdlib.h>
 #include <time.h>
 #include <iostream>

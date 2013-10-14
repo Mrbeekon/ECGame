d4d978e915e0417e19fcb3a964d2f9a0a664c9bd
Isaac Polloni <yuiop1620@hotmail.co.uk>
2013-10-14 19:11:54 +0100

    Removed somewhat unnecessary wchar type

--- include/stdafx.h
+++ include/stdafx.h
@@ -5,7 +5,6 @@ typedef char                sbyte;      // Signed byte
 typedef unsigned short      ushort;     // Unsigned short
 typedef unsigned int        uint;       // Unsigned int
 typedef unsigned long       ulong;      // Unsigned long
-typedef wchar_t             wchar;      // A Unicode Character (Wide Character)
 typedef ushort              WORD;       // A 16-bit word
 typedef uint                DWORD;      // A 32-bit word
 typedef unsigned long long  QWORD;      // A 64-bit word

3c512c93548111be22392b227e5c2c0fa8e2907d
Isaac Polloni <yuiop1620@hotmail.co.uk>
2013-11-26 22:05:12 +0000

    Special Events now explained better than ever

--- include/SEvent.hpp
+++ include/SEvent.hpp
@@ -36,28 +36,34 @@
  * when necessary.
  */
 
+// _ArgT represents our EventArgs. Though it can be any type,
+// it's PREFERABLY some form of EventArgs struct.
 template <typename _ArgT>
 class SEvent
 {
+    // This is purely so that we don't have to type
+    // stuff like "std::vector<std::function<void(_ArgT&)>>::iterator"
     typedef std::function<void(_ArgT&)> _func_t;
     typedef std::vector<_func_t> _funcv_t;
 
+    // Make our list of functions
     _funcv_t _funcs;
 
-    inline void _caller(_func_t f, _ArgT& e)
-    {
-        f(e);
-    }
+    // Saves syntax complications
+    inline void _caller(_func_t f, _ArgT& e) { f(e); }
 
 public:
+    // Allows us to add event functions by doing += to the event instance
     SEvent& operator+=(const _func_t& f)
     {
         _funcs.push_back(f);
         return *this;
     }
 
+    // Allows us to call our event by typing () after the event instance
     SEvent& operator()(_ArgT& e)
     {
+        // Go through all of our event functions and call them
         for(typename _funcv_t::iterator it = _funcs.begin();
             it != _funcs.end(); ++it) {
             _caller(*it, e);
